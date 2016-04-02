dc90eaebac66246ac8c8a7881e4c79445c31ddb6
System User <samuelsaxofon@gmail.com>
2016-04-02 12:36:20 -0500

    dojo Angular 2

---
+++ app/mock-heroes.js
@@ -0,0 +1,25 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var Heroes;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("Heroes", Heroes = [
+                { "id": 11, "name": "Batman" },
+                { "id": 15, "name": "Flash" },
+                { "id": 16, "name": "Deadpool" },
+                { "id": 17, "name": "Iron man" },
+                { "id": 18, "name": "Capitan America" },
+                { "id": 19, "name": "Mujer maravilla" },
+                { "id": 20, "name": "Thor" }
+            ]);
+        }
+    }
+});
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/ 
+//# sourceMappingURL=mock-heroes.js.map
---
+++ app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import {Hero} from './hero';
+
+export var Heroes: Hero[] = [
+	{"id":11, "name": "Batman"},
+	{"id":15, "name": "Flash"},
+	{"id":16, "name": "Deadpool"},
+	{"id":17, "name": "Iron man"},
+	{"id":18, "name": "Capitan America"},
+	{"id":19, "name": "Mujer maravilla"},
+	{"id":20, "name": "Thor"}
+]
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
